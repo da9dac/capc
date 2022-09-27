a7e4a73548cc455eb6486da37d41f81057bb9d09
da9dac <102796152+da9dac@users.noreply.github.com>
2022-09-27 23:47:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ passportConfig(passport);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8001);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
